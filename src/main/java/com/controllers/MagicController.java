2eb559df034b35c333cb9b2128dffcd1a8b0cf58
Tyler Atkinson <tylerh.atkinson@gmail.com>
2018-08-23 13:58:13 -0400

    Trying to connect to database

--- src/main/java/com/controllers/MagicController.java
+++ src/main/java/com/controllers/MagicController.java
@@ -1,5 +1,6 @@
 package com.controllers;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,5 +17,18 @@ public class MagicController {
     @RequestMapping(value= {"/status"}, method = GET)
     public ResponseEntity<String> getStatus() {
         return new ResponseEntity<>("Server is online", new HttpHeaders(), HttpStatus.OK);
+    }
+    @RequestMapping(value={"/connect"}, method = GET)
+    public ResponseEntity<?> connect() {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return new ResponseEntity<>(dbUrl, new HttpHeaders(), HttpStatus.OK);
     }
 }
