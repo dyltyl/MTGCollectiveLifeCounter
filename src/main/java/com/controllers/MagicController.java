36817224c4d193a683c8713b1d3c3f6201db29d3
Tyler Atkinson <tylerh.atkinson@gmail.com>
2018-08-29 14:48:40 -0400

    Revert "added print"
    This reverts commit 89444d7a5cdbb52a9fe3280553202e6d752ba471.

--- src/main/java/com/controllers/MagicController.java
+++ src/main/java/com/controllers/MagicController.java
@@ -351,7 +351,6 @@ public class MagicController {
         @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
         protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
             String bodyOfResponse = Application.getJson(request, true);
-            System.out.println(bodyOfResponse);
             return handleExceptionInternal(ex, bodyOfResponse,
                     new HttpHeaders(), HttpStatus.CONFLICT, request);
         }
