f5b7a94bdeb12e3124ebf642502e6e0ac3d5f4f7
Dylan Menchetti <dylan.menchetti@gmail.com>
2018-09-06 13:04:24 -0400

    Waiting_Lobby || Refresh Page implemented

--- Frontend/lobbyCreation.js
+++ Frontend/lobbyCreation.js
@@ -1,11 +1,12 @@
-//Apply PArtner Commanders when applicable 
+const getAllUrl = 'https://magic-database.herokuapp.com/players';
+const root = document.getElementById('root');
 
 var lSPlayerName = localStorage.getItem('playerName');
 var lSCommanderName = localStorage.getItem('commanderName');
 var lSCommanderTwo = localStorage.getItem('partnerName');
 var gameSize = localStorage.getItem('gameSize');
 var baseLife = localStorage.getItem('baseLife');
-var root = document.getElementById('root');
+// var root = document.getElementById('root');
 
 function checkLog(){
     console.log('Creator Name: ' + lSPlayerName);
@@ -34,9 +35,9 @@ function createPlayerSlot(name, commanderOne, commanderTwo){
     playerSlot.setAttribute('class','playerSlot');
 
     if (lSCommanderTwo === undefined){
-        var playerText = document.createTextNode(lSPlayerName + ' || ' + lSCommanderName);
+        var playerText = document.createTextNode(name + ' || ' + commanderOne);
     }else{
-       var playerText = document.createTextNode(lSPlayerName + ' || ' + lSCommanderName + " || "  + lSCommanderTwo);
+       var playerText = document.createTextNode(name + ' || ' + commanderOne + " || "  + commanderTwo);
     }
     playerSlot.appendChild(playerText);
     // playerSlot.appendChild(c1);
@@ -44,11 +45,56 @@ function createPlayerSlot(name, commanderOne, commanderTwo){
     return playerSlot;
 }
 
-function createPlayerSlots(){
+function createNameSlot(name){
+    var nameSlot = document.createElement('div');
+    nameSlot.setAttribute('class','playerSlot');
+    var nameText = document.createTextNode(name);
+    nameSlot.appendChild(nameText);
+    return nameSlot;
+}
+
+function createWaitingSlots(){
     for(i=0; i<gameSize-1; i++){
         root.appendChild(createEmptySlot());
     }
 }
 
-root.appendChild(createPlayerSlot(lSPlayerName,lSCommanderName,lSCommanderTwo));
-createPlayerSlots();
+/**
+ * fetches getAllPlayers() and for each inserts them into the empty waiting lobby slots
+ */
+function playerRefresh(){
+    var dbGAR = new Array();
+    const requestBody={
+        method: 'GET',
+        headers:{
+            "content-type": "application/json; charset=UTF-8",
+            gameId: localStorage.getItem('gameName')
+        }
+    };
+    fetch(getAllUrl,requestBody)
+    .then(function(response){ return response.json();})
+    .then(function(data){
+        for(var i = 0; i<data.length; i++){
+            console.log(data);
+            console.log(JSON.stringify(data));
+            dbGAR.push(data[i]);
+            console.log('dbGAR size inside function: ' + dbGAR.length);
+        }
+        for(var i = 0; i<dbGAR.length;i++){
+            console.log('hello?');
+            console.log(dbGAR[i].name);
+            root.appendChild(createNameSlot(dbGAR[i].name));
+        }
+        if(dbGAR.length<localStorage.getItem('gameSize')){
+            for(var i =0; i<localStorage.getItem('gameSize')-dbGAR.length;i++){
+                root.appendChild(createEmptySlot());
+            }
+        }
+    })
+    .then(res=>{console.log(res)})
+    .catch(error=>console.log(error))
+}
+
+// root.appendChild(createPlayerSlot(lSPlayerName,lSCommanderName,lSCommanderTwo));
+// createWaitingSlots();
+playerRefresh();
