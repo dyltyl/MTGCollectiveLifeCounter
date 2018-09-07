61dfdf2a5fffe5ebe987e2170d8142292e12e4c5
Dylan Menchetti <dylan.menchetti@gmail.com>
2018-09-07 14:50:18 -0400

    testPush

--- Frontend/playerCreation.js
+++ Frontend/playerCreation.js
@@ -25,7 +25,7 @@ function insertLink(){
         entranceLink.appendChild(document.createTextNode('Enter Match'));
         entrance.appendChild(entranceLink);
         joinLink.appendChild(entrance);
-        // document.getElementById('cP').style.display = 'none';
+        document.getElementById('cP').style.display = 'none';
     }
 }
 /**
