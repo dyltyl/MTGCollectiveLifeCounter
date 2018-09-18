451dc92dcd4656e906f2594c2b9558ef90a7257b
Tyler Atkinson <tylerh.atkinson@gmail.com>
2018-09-18 12:03:17 -0400

    Added routing

--- Frontend-MTG/src/app/app.module.ts
+++ Frontend-MTG/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +8,14 @@ import { GameCreationComponent } from './game-creation/game-creation.component';
 import { PlayerCreationComponent } from './player-creation/player-creation.component';
 import { WaitingLobbyComponent } from './waiting-lobby/waiting-lobby.component';
 import { GameStateComponent } from './game-state/game-state.component';
+
+const appRoutes: Routes = [
+  { path: 'GameCreation', component: GameCreationComponent },
+  { path: 'PlayerCreation', component: PlayerCreationComponent },
+  { path: 'WaitingLobby', component: WaitingLobbyComponent },
+  { path: 'GameState', component: GameStateComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +27,8 @@ import { GameStateComponent } from './game-state/game-state.component';
     GameStateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
