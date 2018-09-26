8e83dc3982c7b11b0ef6a5466fe00a7f2e7cf439
Tyler Atkinson <tylerh.atkinson@gmail.com>
2018-09-26 10:51:42 -0400

    Removed app.component.tns.ts and adjusted formatting

--- Frontend-MTG/src/app/data.service.ts
+++ Frontend-MTG/src/app/data.service.ts
@@ -15,56 +15,62 @@ export class DataService {
    * @param isHost True if the current player is the host
    */
   public setIsHost(isHost: boolean) {
-    if (localStorage !== undefined)
+    if (localStorage !== undefined) {
       localStorage.setItem('isHost', JSON.stringify(isHost));
-    else
+    } else {
       this.host = isHost;
+    }
   }
   /**
    * Retrieves the isHost value from local storage
    */
   public isHost(): boolean {
-    if (localStorage !== undefined)
+    if (localStorage !== undefined) {
       return localStorage.getItem('isHost') === 'true';
-    else
+    } else {
       return this.host;
+    }
   }
   /**
    * Sets the current game in local storage
    * @param game The current game
    */
   public setGame(game: Game) {
-    if (typeof localStorage !== 'undefined')
+    if (typeof localStorage !== 'undefined') {
       localStorage.setItem('game', JSON.stringify(game));
-    else
+    } else {
       this.myGame = game;
+    }
   }
   /**
    * Retrieves the current game from local storage
    */
   public getGame(): Game {
-    if (localStorage !== undefined)
+    if (localStorage !== undefined) {
       return JSON.parse(localStorage.getItem('game'));
-    else 
+    } else {
       return this.myGame;
+    }
   }
   /**
    * Sets the current player in local storage
    * @param player The current player
    */
   public setCurrentPlayer(player: Player) {
-    if (localStorage !== undefined)
+    if (localStorage !== undefined) {
       localStorage.setItem('currentPlayer', JSON.stringify(player));
-    else
+    } else {
       this.myPlayer = player;
+    }
   }
   /**
    * Retrieves the current player from local storage
    */
   public getCurrentPlayer(): Player {
-    if (localStorage !== undefined)
+    if (localStorage !== undefined) {
       return JSON.parse(localStorage.getItem('currentPlayer'));
-    else
+    } else {
       return this.myPlayer;
+    }
   }
 }
