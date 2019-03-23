c302820dd485c79aead6711245c8202eee267eb4
Tyler <tylerh.atkinson@gmail.com>
2019-03-23 18:03:49 -0400

    Started player controller base

---
+++ Backend/MTGCollectiveLifeCounterBackend/Controllers/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MTGCollectiveLifeCounterBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTGCollectiveLifeCounterBackend.Controllers {
+    [EnableCors("*")]
+    [Route("[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase {
+        [HttpGet]
+        public ActionResult<Player> Get() {
+            Player player = new Player {
+                Name = "Tyler"
+            };
+            return player;
+        }
+    }
+}
