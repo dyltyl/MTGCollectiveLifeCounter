0c6f0653381232187518e13bcfae523562fe8a6d
Tyler <tylerh.atkinson@gmail.com>
2019-05-06 22:49:45 -0400

    Added init to setup database connection

--- Backend/Database/DatabaseInstaller.py
+++ Backend/Database/DatabaseInstaller.py
@@ -2,44 +2,58 @@ import psycopg2
 import os
 
 
-def connect_to_database(): #Todo: add null checks/etc
-    parts = os.environ['DATABASE_URL'].split(":")
-    username = parts[1][2:]
-    password = parts[2][0:parts[2].index("@")]
-    database = parts[3][parts[3].index("/")+1:]
+class DatabaseInstaller:
+    def __init__(self):
+        parts = os.environ['DATABASE_URL'].split(":")
+        self.username = parts[1][2:]
+        self.password = parts[2][0:parts[2].index("@")]
+        self.database = parts[3][parts[3].index("/") + 1:]
 
-    return psycopg2.connect("dbname="+database+" user="+username+" password="+password)
+        print('username: ' + self.username)
+        print('password: ' + self.password)
+        print('database: ' + self.database)
+
+        try:
+            database = self.connect_to_database()
+            database.close()
+        except psycopg2.OperationalError:
+            raise Exception('Cannot connect to database')
 
 
-def check_table_exists(table):
-    database = connect_to_database()
-    cursor = database.cursor()
-    cursor.execute("SELECT 1 FROM information_schema.tables WHERE table_name = '"+table+"';")
-    result = cursor.fetchone()[0] == 1
-    cursor.close()
-    database.close()
-    return result
+    def connect_to_database(self): #Todo: add null checks/etc
+        return psycopg2.connect("dbname="+self.database+" user="+self.username+" password="+self.password)
 
 
-def create_table(tablename, filename):
-    if not check_table_exists(tablename):
-        database = connect_to_database()
-        sql_file = open(filename, "r")
-        sql = sql_file.read()
-        sql_file.close()
+    def check_table_exists(self, table):
+        database = self.connect_to_database()
         cursor = database.cursor()
-        cursor.execute(sql)
+        cursor.execute("SELECT 1 FROM information_schema.tables WHERE table_name = '"+table+"';")
+        result = cursor.fetchone()[0] == 1
         cursor.close()
-    else:
-        print(tablename + ' needs updating')
+        database.close()
+        return result
 
 
-def setup_tables():
-    create_table('players', 'createPlayers.sql')
-    create_table('games', 'createGames.sql')
-    create_table('commanders', 'createCommanders.sql')
-    create_table('commander_damage', 'createCommanderDamage.sql')
-    create_table('life', 'createLife.sql')
+    def create_table(self, tablename, filename):
+        if not self.check_table_exists(tablename):
+            database = self.connect_to_database()
+            sql_file = open(filename, "r")
+            sql = sql_file.read()
+            sql_file.close()
+            cursor = database.cursor()
+            cursor.execute(sql)
+            cursor.close()
+        else:
+            print(tablename + ' needs updating')
 
 
-setup_tables()
+    def setup_tables(self):
+        self.create_table('players', 'createPlayers.sql')
+        self.create_table('games', 'createGames.sql')
+        self.create_table('commanders', 'createCommanders.sql')
+        self.create_table('commander_damage', 'createCommanderDamage.sql')
+        self.create_table('life', 'createLife.sql')
+
+
+installer = DatabaseInstaller()
+installer.setup_tables()
