a4f62188cc95bf2f2cb276e57031554f3316bcf6
Tyler <tylerh.atkinson@gmail.com>
2019-05-06 22:12:57 -0400

    That didn't work

--- Backend/Database/DatabaseInstaller.py
+++ Backend/Database/DatabaseInstaller.py
@@ -1,6 +1,5 @@
-import os
-os.system("python -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --trusted-host pypi.python.org psycopg2")
 import psycopg2
+import os
 
 
 def connect_to_database(): #Todo: add null checks/etc
